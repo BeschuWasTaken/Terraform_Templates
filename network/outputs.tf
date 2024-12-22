b18ee132b7070a5add0f3fb8d535f2c2483eb670
BeschuWasTaken <bendik.s.engedal@hotmail.com>
2024-12-22 23:38:29 +0100

    Delete network directory

--- network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip_address" {
-  value = azurerm_public_ip.public_ip.ip_address
-  description = "The public IP address"
-}
-
-output "network_interface_id" {
-  value = azurerm_network_interface.nic.id
-  description = "The ID of the network interface"
-}
